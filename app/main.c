e6ebb668c39736e6219e42dc1436051b131e1222
dreamcode <181860068@smail.nju.edu.cn>
2020-05-01 12:24:37 +0800

    new testcase

--- app/main.c
+++ app/main.c
@@ -1,6 +1,6 @@
 #include "lib.h"
 #include "types.h"
-
+/*
 int uEntry(void)
 {
 	char ch;
@@ -31,5 +31,50 @@ int uEntry(void)
 		}
 	}
 	exit();
+	return 0;
+}
+*/
+/*
+int uEntry(void){
+	int data = 2020;
+	int data1 = 1000;
+	int i = 4;
+	int ret = fork();
+	if(ret == 0){
+		while(i!=0){
+			i--;
+			printf("Child Process: %d, %d\n", data, data1);
+			write(SH_MEM, (uint8_t*)&data,4,0);
+			data += data1;
+			sleep(128);
+		}
+		exit();
+	}
+	else if(ret !=-1){
+		while(i !=0){
+			i--;
+			read(SH_MEM, (uint8_t*)&data1,4,0);
+			printf("Father Process: %d, %d\n",data,data1);
+			sleep(128);
+		}
+		exit();
+	}
+	return 0;
+}
+*/
+
+int uEntry(void){
+	int dec = 0;
+	int hex = 0;
+	char str[6];
+	char cha = 0;
+	int ret = 0;
+	while(1){
+		printf("Input:\" Test %%c Test %%6s %%d %%x\"\n");
+		ret = scanf(" Test %c Test %6s %d %x",&cha,str,&dec, &hex);
+		printf("Ret: %d; %c, %s, %d, %x.\n",ret,cha,str,dec,hex);
+		if(ret == 4)
+		break;
+	}
 	return 0;
 }
